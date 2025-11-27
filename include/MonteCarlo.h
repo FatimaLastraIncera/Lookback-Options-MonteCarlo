ec5f82d3ad00fb89e8c4ab7974471a20267f4840
Fátima Lastra Incera <lastraincerafatima@gmail.com>
2025-11-27 11:50:45 +0100

    Update MonteCarlo.h

--- include/MonteCarlo.h
+++ include/MonteCarlo.h
@@ -1,1 +1,9 @@
+#pragma once
 
+#include <cstddef>
+#include "LookbackPricer.h"
+
+MCResult monteCarloFloatingLookbackPrice(const GBMParams& params,
+                                         std::size_t nPaths,
+                                         LookbackType type,
+                                         unsigned long seed = 5489UL);
