105285022f21196883f8ea7fb017e49a039b3730
Fátima Lastra Incera <lastraincerafatima@gmail.com>
2025-11-27 11:49:24 +0100

    Update LookbackPricer.h

--- include/LookbackPricer.h
+++ include/LookbackPricer.h
@@ -1,5 +1,38 @@
-#ifndef OPTIONPRICER_H
-#define OPTIONPRICER_H
+#pragma once
 
-#include "Option.h"
-#include <vector>
+#include <cstddef>
+
+// ---------------------------
+//   GBM PARAMETERS
+// ---------------------------
+struct GBMParams {
+    double S0;
+    double r;
+    double sigma;
+    double T;
+    int steps;
+};
+
+// ---------------------------
+//   LOOKBACK TYPES
+// ---------------------------
+enum class LookbackType {
+    FloatingCall,
+    FloatingPut
+};
+
+// ---------------------------
+//   MONTE CARLO RESULT
+// ---------------------------
+struct MCResult {
+    double price;
+    double stdError;
+};
+
+// ---------------------------
+//   PRICER PROTOTYPE
+// ---------------------------
+MCResult monteCarloFloatingLookbackPrice(const GBMParams& params,
+                                         std::size_t nPaths,
+                                         LookbackType type,
+                                         unsigned long seed = 5489UL);
